11f3f7256893741f899e82c48e13e992c11999c5
Анваров Алмаз Салимович <laserbeak.des@gmail.com>
2018-08-19 02:07:24 +0300

    WR-3 Классы конфигураций.

--- src/main/java/com/aincorp/wardrobe/starter/StarterApplication.java
+++ src/main/java/com/aincorp/wardrobe/starter/StarterApplication.java
@@ -2,10 +2,18 @@ package com.aincorp.wardrobe.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.persistence.Entity;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableJpaRepositories(basePackages = "com.aincorp.wardrobe.starter.dao")
+@EntityScan(basePackages = "com.aincorp.wardrobe.starter.model")
+@EnableWebSecurity
 public class StarterApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/aincorp/wardrobe/starter/config/MvcConfig.java
+++ src/main/java/com/aincorp/wardrobe/starter/config/MvcConfig.java
@@ -2,6 +2,9 @@ package com.aincorp.wardrobe.starter.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -9,4 +12,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
 
+    public void addViewControllers(ViewControllerRegistry registry) {
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 }
