34a5705b889f37e25ae8f839cd1567e1a5de1573
Christina Ivanova <icha888k@gmail.com>
2018-08-25 19:48:03 +0300

    WR-7 Подключение AngularJS, добавление тестового контроллера

---
+++ src/main/java/com/aincorp/wardrobe/springboot/controllers/TestController.java
@@ -0,0 +1,21 @@
+package com.aincorp.wardrobe.springboot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+public class TestController {
+
+  @RequestMapping("/resource")
+  public Map<String,Object> home() {
+    Map<String,Object> model = new HashMap<String,Object>();
+    model.put("id", UUID.randomUUID().toString());
+    model.put("content", "Hello World");
+    return model;
+  }
+
+}
